3d56caf2546cac53d2ecee568455909a16f40829
ManvitSai1 <92364927+ManvitSai1@users.noreply.github.com>
2024-05-04 16:13:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 sound1="";
 sound2="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
 function preload(){
-    song = loadsound("music.mp3");
-    song = loadsound("music2.mp3");
+    song = loadSound("music.mp3");
+    song = loadSound("music2.mp3");
 }
 function setup()
 {
@@ -10,12 +14,32 @@ function setup()
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
-    
+    poseNet = ml5.poseNet(video, modelLoaded());
+    poseNet.on('pose', gotPoses());
+}
+function modelLoaded()
+{
+    console.log("poseNet is initialised");
+}
+function gotPoses(results)
+{
+    if(results.length>0)
+        {
+            console.log(results);
+            leftWristX=results[0].pose.leftWrist.x;
+            leftWristY=results[0].pose.leftWrist.y;
+            console.log("leftWristX= " + leftWristX + "lefWristY=" + leftWristY);
+            rightWristX=results[0].pose.rightWrist.x;
+            rightWristY=results[0].pose.rightWrist.y;
+            console.log("rightWristX= " + rightWristX + "rightWristY=" + rightWristY);
+        }
+
 }
 function draw()
 {
     Image(video, 0, 0, 600, 500);
 }
-funcion play(){
+function play()
+{
     song.play();
 }
